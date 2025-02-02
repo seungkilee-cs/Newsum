bfa98461a9c2ee9568525397d62e197669525173
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-02-02 14:37:36 +0900

    exclude api key lol

--- scraper/summarizer.py
+++ scraper/summarizer.py
@@ -5,7 +5,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def generate_summary(article_content, max_tokens=150):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "Summarize the following article in 3 brief bullet points, each bullet points less than 20 words each:"},
             {"role": "user", "content": article_content}
