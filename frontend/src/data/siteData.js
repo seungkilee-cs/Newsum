32939167abf942eb9b6b041b966e49c10e330bb5
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-03-10 15:34:26 +0900

    autodeploy-2025-03-10_15_34_17

--- frontend/src/data/siteData.js
+++ frontend/src/data/siteData.js
@@ -1,16 +1,14 @@
-import images from "../assets";
-
 export const sites = [
   {
     name: "American Liberty Media",
     url: "https://www.americanlibertymedia.com",
     image: "../assets/ALM.avif",
   },
-  // {
-  //   name: "CNN",
-  //   url: "https://www.cnn.com",
-  //   image: "../assets/CNN.svg",
-  // },
+  {
+    name: "CNN",
+    url: "https://www.cnn.com",
+    image: "../assets/CNN.svg",
+  },
   {
     name: "Fox News",
     url: "https://www.foxnews.com/",
