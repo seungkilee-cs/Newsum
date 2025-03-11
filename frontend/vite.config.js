2276819325cf3e6eafd1f5505d12f56be10e9ae0
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-03-11 17:03:27 +0900

    updated base for vite config-2025-03-11_17_03_12

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Newsum/", // Replace <repo-name> with your GitHub
   define: {
     "import.meta.env.VITE_APP_ENVIRONMENT": JSON.stringify(
       process.env.VITE_APP_ENVIRONMENT,
