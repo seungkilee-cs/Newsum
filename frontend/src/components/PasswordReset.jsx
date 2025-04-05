2dc6396fdcb39c0d8448e21bbe71bb7c631736e1
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-04-05 18:40:23 +0900

    added navigate to password reset-2025-04-05_18_40_05

--- frontend/src/components/PasswordReset.jsx
+++ frontend/src/components/PasswordReset.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "../styles/PasswordReset.css"; // Import the CSS file
+import { useNavigate } from "react-router-dom";
 
 function PasswordReset() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,20 +26,27 @@ function PasswordReset() {
   };
 
   return (
-    <div className="password-reset-container">
-      <form className="password-reset-form" onSubmit={handleSubmit}>
-        <h2>Reset Password</h2>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-          required
-        />
-        <button type="submit">Send Reset Link</button>
-        {message && <p className="success-message">{message}</p>}
-        {error && <p className="error-message">{error}</p>}
-      </form>
+    <div className="password-reset-wrapper">
+      <div className="password-reset-container">
+        <form className="password-reset-form" onSubmit={handleSubmit}>
+          <h2>Reset Password</h2>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+          <button type="submit">Send Reset Link</button>
+          {message && <p className="success-message">{message}</p>}
+          {error && <p className="error-message">{error}</p>}
+        </form>
+      </div>
+      <div className="home-btn">
+        <button onClick={handleGoHome} className="btn-home">
+          Go Back Home
+        </button>
+      </div>
     </div>
   );
 }
